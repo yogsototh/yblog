76f8f941f4695259bcfff31f0482a42bbfb6f3e8
Yann Esposito (Yogsototh) <Yann.Esposito@gmail.com>
2013-01-25 22:57:44 +0100

    cleaning up the code

---
+++ Abbreviations.hs
@@ -0,0 +1,28 @@
+module Abbreviations
+  (abbreviationFilter)
+where
+
+import           Data.Map       (Map)
+import qualified Data.Map    as M
+import           Hakyll
+
+--------------------------------------------------------------------------------
+abbreviationFilter :: String -> String
+abbreviationFilter = replaceAll "%[a-zA-Z0-9_]*" newnaming
+  where
+    newnaming matched = case M.lookup (tail matched) abbreviations of
+                          Nothing -> matched
+                          Just v -> v
+
+--------------------------------------------------------------------------------
+abbreviations :: Map String String
+abbreviations = M.fromList
+    [ ("TLDR",   "Too long; didn't read")
+    , ("PEBKAC", "Problem exists between keyboard and chair")
+    , ("html", "<span class=\"sc\">html</span>")
+    , ("css", "<span class=\"sc\">css</span>")
+    , ("svg", "<span class=\"sc\">svg</span>")
+    , ("test", "Just a test")
+    , ("latex", "<span style=\"text-transform: uppercase\">L<sup style=\"vertical-align: 0.15em; margin-left: -0.36em; margin-right: -0.15em; font-size: .85em\">a</sup>T<sub style=\"vertical-align: -0.5ex; margin-left: -0.1667em; margin-right: -0.125em; font-size: 1em\">e</sub>X</span>")
+    , ("xelatex", "<span style=\"text-transform: uppercase\">X<sub style=\"vertical-align: -0.5ex; margin-left: -0.1667em; margin-right: -0.125em; font-size: 1em\">&#x018E;</sub>L<sup style=\"vertical-align: 0.15em; margin-left: -0.36em; margin-right: -0.15em; font-size: .85em\">a</sup>T<sub style=\"vertical-align: -0.5ex; margin-left: -0.1667em; margin-right: -0.125em; font-size: 1em\">e</sub>X</span>")
+    ]
--- site.hs
+++ site.hs
@@ -7,17 +7,45 @@ import           Hakyll
 import           Data.Map             (Map)
 import qualified Data.Map             as M
 
+import           Abbreviations        (abbreviationFilter)
+import           Multilang            (multiContext)
+
+
+--------------------------------------------------------------------------------
+-- Simply copy in the right place
+staticBehavior :: Rules ()
+staticBehavior = do
+  route   idRoute
+  compile copyFileCompiler
+
+--------------------------------------------------------------------------------
+-- change the extension to html
+-- prefilter the markdown
+-- apply pandoc (markdown -> html)
+-- postfilter the html
+-- apply templates posts then default then relitivize url
+markdownBehavior :: Rules ()
+markdownBehavior = do
+  route $ setExtension "html"
+  compile $ do
+    body <- getResourceBody
+    return $ renderPandoc (fmap preFilters body)
+    >>= applyFilter postFilters
+    >>= loadAndApplyTemplate "templates/post.html"    postCtx
+    >>= loadAndApplyTemplate "templates/default.html" postCtx
+    >>= relativizeUrls
+  where
+    applyFilter f str = return $ (fmap $ f) str
+    preFilters :: String -> String
+    preFilters = abbreviationFilter
+    postFilters :: String -> String
+    postFilters = id
 
 --------------------------------------------------------------------------------
 main :: IO ()
 main = hakyll $ do
-    match "Scratch/images/*" $ do
-        route   idRoute
-        compile copyFileCompiler
-
-    match "Scratch/js/*" $ do
-        route   idRoute
-        compile copyFileCompiler
+    match "Scratch/images/*" staticBehavior
+    match "Scratch/js/*"     staticBehavior
 
     match "Scratch/css/*" $ do
         route   $ setExtension "css"
@@ -25,20 +53,13 @@ main = hakyll $ do
           withItemBody (unixFilter "sass" ["--trace"]) >>=
           return . fmap compressCss
 
+    match "Scratch/posts/en/*" markdownBehavior
+    match "Scratch/posts/fr/*" markdownBehavior
+
     match (fromList ["Scratch/about.rst", "Scratch/contact.markdown"]) $ do
         route   $ setExtension "html"
         compile $ pandocCompiler
             >>= loadAndApplyTemplate "templates/default.html" yDefaultContext
-            >>= relativizeUrls
-
-    match "Scratch/posts/en/*" $ do
-        route $ setExtension "html"
-        compile $ do
-            body <- getResourceBody
-            return $ renderPandoc (fmap preFilters body)
-            >>= applyFilter postFilters
-            >>= loadAndApplyTemplate "templates/post.html"    postCtx
-            >>= loadAndApplyTemplate "templates/default.html" postCtx
             >>= relativizeUrls
 
     create ["Scratch/archive.html"] $ do
@@ -55,16 +76,6 @@ main = hakyll $ do
                 >>= relativizeUrls
 
 
-    match "Scratch/index.html" $ do
-        route idRoute
-        compile $ do
-            let indexCtx = field "posts" $ \_ -> postList (take 3 . recentFirst)
-
-            getResourceBody
-                >>= applyAsTemplate indexCtx
-                >>= loadAndApplyTemplate "templates/default.html" postCtx
-                >>= relativizeUrls
-
     match "index.html" $ do
         route idRoute
         compile $ do
@@ -78,109 +89,14 @@ main = hakyll $ do
     match "templates/*" $ compile templateCompiler
 
 
-
---------------------------------------------------------------------------------
-
-preFilters :: String -> String
-preFilters = abbreviationFilter
-
-postFilters :: String -> String
-postFilters = id
-
-applyFilter f str = return $ (fmap $ f) str
-
-abbreviationFilter :: String -> String
-abbreviationFilter = replaceAll "%[a-zA-Z0-9_]*" newnaming
-  where
-    newnaming matched = case M.lookup (tail matched) abbreviations of
-                          Nothing -> matched
-                          Just v -> v
-
 --------------------------------------------------------------------------------
 yDefaultContext = metaKeywordContext <>
-                  languageContext <>
+                  multiContext <>
                   prefixContext <>
-                  otherlanguageContext <>
-                  tradsContext <>
                   defaultContext
 
 --------------------------------------------------------------------------------
--- ABBREVIATIONS
-
---------------------------------------------------------------------------------
-applyAbbrs :: Item String -> Compiler (Item String)
-applyAbbrs = applyAsTemplate abbrContext
-
---------------------------------------------------------------------------------
-abbrContext :: Context a
-abbrContext = functionField "abbr" $ \args _ ->
-    case args of
-        [k] -> case M.lookup k abbreviations of
-            Just v  -> return v
-            Nothing -> fail $ "Abbreviation not found: " ++ k
-        _   -> fail "Wrong args for abbreviations"
-
-
---------------------------------------------------------------------------------
-abbreviations :: Map String String
-abbreviations = M.fromList
-    [ ("TLDR",   "Too long; didn't read")
-    , ("PEBKAC", "Problem exists between keyboard and chair")
-    , ("html", "<span class=\"sc\">html</span>")
-    , ("css", "<span class=\"sc\">css</span>")
-    , ("svg", "<span class=\"sc\">svg</span>")
-    , ("test", "Just a test")
-    , ("latex", "<span style=\"text-transform: uppercase\">L<sup style=\"vertical-align: 0.15em; margin-left: -0.36em; margin-right: -0.15em; font-size: .85em\">a</sup>T<sub style=\"vertical-align: -0.5ex; margin-left: -0.1667em; margin-right: -0.125em; font-size: 1em\">e</sub>X</span>")
-    , ("xelatex", "<span style=\"text-transform: uppercase\">X<sub style=\"vertical-align: -0.5ex; margin-left: -0.1667em; margin-right: -0.125em; font-size: 1em\">&#x018E;</sub>L<sup style=\"vertical-align: 0.15em; margin-left: -0.36em; margin-right: -0.15em; font-size: .85em\">a</sup>T<sub style=\"vertical-align: -0.5ex; margin-left: -0.1667em; margin-right: -0.125em; font-size: 1em\">e</sub>X</span>")
-    ]
-
---------------------------------------------------------------------------------
-data Trad = Trad { frTrad :: String, enTrad :: String }
-trads :: Map String Trad
-trads = M.fromList $ map toTrad [
-         ("changeLanguage",("En Français","In English"))
-        ,("switchCss",("Changer de theme","Change Theme"))
-        ,("loading",("Chargement en cours","Loading"))
-        ,("welcome",("Bientôt","Soon"))
-        ]
-        where
-          toTrad (k,(f,e)) = (k, Trad { frTrad = f, enTrad = e })
-
-
---------------------------------------------------------------------------------
-tradsContext = functionField "trad" $ \args item -> do
-                k <- getArgs args
-                v <- getValue k trads
-                lang <- itemLang item
-                case lang of
-                  "en" -> return (enTrad v)
-                  "fr" -> return (frTrad v)
-                  _    -> fail $ lang ++ " is not a supported language"
-                where
-                  getArgs [k] = return k
-                  getArgs _   = fail "Wrong arg for trad"
-                  getValue key hmap = case M.lookup key hmap of
-                                        Just value -> return value
-                                        Nothing -> fail "Traduction not found"
-
-
---------------------------------------------------------------------------------
 prefixContext = field "webprefix" $ \_ -> return $ "/Scratch"
-
---------------------------------------------------------------------------------
-otherlanguageContext = field "otherlanguage" $ \item -> do
-  lang <- itemLang item
-  return $ if (lang == "en") then "fr" else "en"
-
-itemLang :: Item a -> Compiler String
-itemLang item = do
-  filepath <- return $ toFilePath (itemIdentifier item)
-  return $ if (languageFromPath filepath == "fr" ) then "fr" else "en"
-    where
-      languageFromPath = take 2 . drop 1
-
---------------------------------------------------------------------------------
-languageContext = field "language" itemLang
 
 --------------------------------------------------------------------------------
 metaKeywordContext = field "metaKeywords" $ \item -> do
@@ -195,7 +111,6 @@ postCtx :: Context String
 postCtx =
     dateField "date" "%B %e, %Y" <>
     yDefaultContext
-
 
 --------------------------------------------------------------------------------
 postList :: ([Item String] -> [Item String]) -> Compiler String
