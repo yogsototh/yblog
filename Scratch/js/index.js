2f646f8f3eab0585c53d11a3f81f8e0212c8c585
Yann Esposito (Yogsototh) <Yann.Esposito@gmail.com>
2013-03-18 11:51:36 +0100

    fix ie and document.write

--- Scratch/js/index.js
+++ Scratch/js/index.js
@@ -68,6 +68,15 @@ var styles=[ pref+'/scientific.css'
 		   , pref+'/modern.css'
 		   , pref+'/darkmodern.css'];
 
+// -- fix for ie (I HATE YOU Internet Explorer!)
+if (!('forEach' in Array.prototype)) {
+    Array.prototype.forEach= function(action, that /*opt*/) {
+        for (var i= 0, n= this.length; i<n; i++)
+            if (i in this)
+                action.call(that, this[i], i, this);
+    };
+}
+
 function badPref() {
     var badPrefix=true;
     styles.forEach(function(trystyle){
