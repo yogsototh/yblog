86f0692fb3526305cf5025f1e44b28e49a42f002
Yann Esposito (Yogsototh) <Yann.Esposito@gmail.com>
2014-05-27 09:28:50 +0200

    better publish scirpt

--- publish.sh
+++ publish.sh
@@ -1,24 +1,3 @@
 #!/usr/bin/env zsh
 
-hakylldir=$HOME/Sites/yblog
-pubdir=$hakylldir/_publish
-
-[[ ! -e $pubdir ]] && git clone -b gh-pages github.com:yogsototh/yannesposito.com.git _publish
-
-cd $hakylldir       && \
-git pull            && \
-./rebuild.sh        && \
-cd $pubdir          && \
-git checkout gh-pages && \
-print -- "get latest modif from github" && \
-git pull && \
-print -- "Remove all files except .git" && \
-\rm -rf * && \
-print -- "Copy _site" && \
-\cp -rf ../_site/* . && \
-print -- "Adding files to repository" && \
-git add . && \
-git add -u && \
-print -- "Commit and publish" && \
-git commit -m "publishing"
-git push
+cabal run publish && ./fastpublish.sh
