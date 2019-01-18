3771395d05d516e5572ed3640bdd2e94b75efd62
Yann Esposito (Yogsototh) <yann.esposito@gmail.com>
2019-01-18 16:42:55 +0100

    image optim CLI

--- tools/imgforweb.sh
+++ tools/imgforweb.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env zsh
-(($#==0)) && {
-    print -- "usage: ${0:t} [file ...]"
-    exit 1
-}
-for fic in $@; do
-	newname="${fic:r}-web.${fic:e}"
-	# convert -thumbnail 1600x1200\> -adaptive-sharpen 0 -quality 75% $fic $newname
-	convert -adaptive-sharpen 0 -quality 75% $fic $newname
-	mv $fic $fic.old
-	mv $newname $fic
-done
+if (($#==0))
+then files=( **/*.{png,jpg,jpeg} )
+else files=( $@ )
+fi
+
+du -h -c $files
+
+maxsize=512
+mogrify -resize ${maxsize}x${maxsize}\> $files
+imageoptim $files
+
+du -h -c $files
+
